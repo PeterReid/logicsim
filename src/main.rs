d304e6b3a0d23191e857674425b142e15a44d4f0
Peter Reid <peter.d.reid@gmail.com>
2015-02-01 10:48:56 -0500

    Split out core simulation stuff from the things I'm building

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 extern crate arena;
 
+mod base;
+
 use arena::Arena;
 
 use std::collections::binary_heap::BinaryHeap;
@@ -9,349 +11,13 @@ use std::cmp::PartialOrd;
 use std::cmp::{Ord, Ordering};
 use std::collections::HashSet;
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
-enum LineState {
-    Low,
-    High,
-    Floating,
-    Conflict,
-}
-
-impl LineState {
-    fn lows_highs_count(self) -> (u32, u32) {
-        match self {
-            LineState::Low => (1, 0),
-            LineState::High => (0, 1),
-            LineState::Floating => (0, 0),
-            LineState::Conflict => (1, 1),
-        }
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub struct NodeIndex(pub usize);
-
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub struct ElementIndex(pub usize);
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-pub struct PropogationDelay(pub u32);
-
-impl PropogationDelay {
-    fn get(self) -> u32 {
-        let PropogationDelay(ticks) = self;
-        ticks
-    }
-}
-
-trait Element {
-    fn step(&self, c: &mut NodeCollection);
-    fn get_nodes(&self) -> Vec<NodeIndex>;
-}
-
-#[derive(Debug, Copy, Clone)]
-struct Link {
-    linked_to: NodeIndex,
-    delay: PropogationDelay,
-    id: u64,
-}
-
-#[derive(Debug, Copy, Clone)]
-struct Influence {
-    force_generator: NodeIndex,
-    force_kind: LineState,
-    force_id: u64,
-}
-
-struct Node {
-    output_state: LineState,
-    linked_with: Vec<Link>,
-    element_index: Option<ElementIndex>,
-    influences: Vec<Influence>
-}
-
-impl Node {
-    fn new() -> Node {
-        Node {
-            output_state: LineState::Floating,
-            linked_with: Vec::new(),
-            element_index: None,
-            influences: Vec::new(),
-        }
-    }
-    
-    fn get_input_state(&self) -> LineState {
-        let mut lows = 0;
-        let mut highs = 0;
-        
-        for influence in self.influences.iter() {
-            let (delta_low, delta_high) = influence.force_kind.lows_highs_count();
-            lows += delta_low;
-            highs += delta_high;
-        }
-    
-        match (lows>0, highs>0) {
-            (false, false) => LineState::Low, // is this physically accurate? I am not sure
-            (true, false) => LineState::Low,
-            (false, true) => LineState::High,
-            (true, true) => LineState::Conflict,
-        }
-    }
-}
-
-#[derive(PartialEq, Eq, Copy, Debug)]
-struct LineStateEvent {
-    node: NodeIndex,
-    new_state: LineState,
-    time: u64,
-    id: u64, // to keep time ties in order
-    forcer: NodeIndex,
-    force_id: u64,
-}
-
-impl PartialOrd<LineStateEvent> for LineStateEvent {
-    fn partial_cmp(&self, other: &LineStateEvent) -> Option<Ordering> {
-        if other.time != self.time {
-            other.time.partial_cmp(&self.time)
-        } else {
-            other.id.partial_cmp(&self.id)
-        }
-    }
-}
-impl Ord for LineStateEvent {
-    fn cmp(&self, other: &LineStateEvent) -> Ordering {
-        if other.time != self.time {
-            other.time.cmp(&self.time)
-        } else {
-            other.id.cmp(&self.id)
-        }
-    }
-}
-
-struct NodeCollection<'a> {
-    nodes: Vec<Node>,
-    events: BinaryHeap<LineStateEvent>,
-    current_tick: u64,
-    elements: Vec<&'a (Element + 'a)>,
-    //element_arena: &'a Arena,
-    event_id_counter: u64,
-    link_id_counter: u64,
-    force_id_counter: u64,
-}
-
-impl<'a> NodeCollection<'a> {
-    fn new() -> NodeCollection<'a> {
-        NodeCollection {
-            //element_arena: arena,
-            nodes: Vec::new(),
-            events: BinaryHeap::new(),
-            current_tick: 0,
-            elements: Vec::new(),
-            event_id_counter: 0,
-            link_id_counter: 0,
-            force_id_counter: 0,
-        }
-    }
-    
-    fn link(&mut self, a: NodeIndex, b: NodeIndex, delay: PropogationDelay) {
-        self.link_id_counter += 1;
-        self.nodes[a.get()].linked_with.push(Link{linked_to: b, delay: delay, id: self.link_id_counter});
-        self.nodes[b.get()].linked_with.push(Link{linked_to: a, delay: delay, id: self.link_id_counter});
-    }
-    
-    fn apply_influence(&mut self, e: &LineStateEvent) {
-    
-        let target = &mut self.nodes[e.node.get()];
-        if let Some(ref mut existing_influence) = target.influences.iter_mut().find(|&: influence| { influence.force_generator == e.forcer }) {
-            if existing_influence.force_id >= e.force_id {
-                return;
-            }
-            existing_influence.force_id = e.force_id;
-            existing_influence.force_kind = e.new_state;
-            return;
-        }
-        
-        // No influence on this node so far.
-        target.influences.push(Influence{
-            force_generator: e.forcer,
-            force_id: e.force_id,
-            force_kind: e.new_state,
-        });
-    }
-    
-    fn play_event(&mut self, e: LineStateEvent) {
-        self.current_tick = e.time;
-        self.apply_influence(&e);
-        
-        let target = &self.nodes[e.node.get()];
-        
-        for adjacent_link in target.linked_with.iter() {
-            let adjacent_node = &self.nodes[adjacent_link.linked_to.get()];
-            let already_influenced = 
-                if let Some(existing) = adjacent_node.influences.iter().find(|&: influence| { influence.force_generator == e.forcer }) {
-                    existing.force_id >= e.force_id
-                } else {
-                    false
-                };
-            if !already_influenced {
-                self.event_id_counter += 1;
-                
-                println!("Propogating from {:?} to {:?} at time {:?} with delay {:?}", e.node, adjacent_link.linked_to, self.current_tick, adjacent_link.delay.get());
-                let evt = LineStateEvent{
-                    node: adjacent_link.linked_to,
-                    new_state: e.new_state,
-                    time: self.current_tick + adjacent_link.delay.get() as u64,
-                    id: self.event_id_counter,
-                    forcer: e.forcer,
-                    force_id: e.force_id,
-                };
-                self.events.push(evt);
-            }
-        }
-    }
-    
-    fn play(&mut self) -> bool {
-        if let Some(evt) = self.events.pop() {
-            println!("Playing event: {:?}", evt);
-            
-            let maybe_element_index = self.nodes[evt.node.get()].element_index;
-            self.play_event(evt);
-            
-            if let Some(element_index) = maybe_element_index{
-                self.elements[element_index.get()].step(self);
-            }
-            return true;
-        } else {
-            println!("No events");
-            return false;
-        }
-    }
-    
-    fn absorb<'b:'a>(&mut self, creator: NodeCreator<'b>) {
-        for element in creator.elements.iter() {
-            self.add_element(*element);
-        }
-        
-        for &(a, b, delay) in creator.links.iter() {
-            self.link(a, b, delay);
-        }
-    }
-    
-    fn add_element(&mut self, elem: &'a (Element + 'a)) {
-    
-        let element_index = ElementIndex(self.elements.len());
-        for node_index in elem.get_nodes().iter() {
-            
-            while self.nodes.len() <= node_index.get() {
-                self.nodes.push(Node::new());
-            }
-        
-            let node = &mut self.nodes[node_index.get()];
-            assert!(node.element_index.is_none(), "An element tried to claim an already-claimed node!");
-            node.element_index = Some(element_index);
-        }
-    
-        self.elements.push(elem);
-        
-    }
-    
-    // We need to run each element in resolve-float mode
-    fn resolve_floats(&mut self) {
-        for i in range(0, self.elements.len()) {
-            self.elements[i].step(self);
-        }
-    }
-}
-
-impl ElementIndex {
-    fn get(self) -> usize {
-        let ElementIndex(idx) = self;
-        idx
-    }
-}
-
-impl NodeIndex {
-    fn get(self) -> usize {
-        let NodeIndex(idx) = self;
-        idx
-    }
-
-    fn set(&mut self, val: usize) {
-        match self {
-            &mut NodeIndex(ref mut x) => {
-                *x = val;
-            }
-        }
-    }
-    
-    fn write(self, new_state: LineState, c: &mut NodeCollection) {
-        if new_state == c.nodes[self.get()].output_state {
-            return; // no-op
-        }
-        self.write_later(new_state, PropogationDelay(0), c)
-    }
-    
-    fn write_later(self, new_state: LineState, delta_time: PropogationDelay, c: &mut NodeCollection) {
-        let node = &mut c.nodes[self.get()];
-        
-        let old_state = node.output_state;
-        
-        node.output_state = new_state;
-        c.event_id_counter += 1;
-        c.force_id_counter += 1;
-        
-        let evt = LineStateEvent{
-            node: self,
-            new_state: new_state,
-            time: c.current_tick + delta_time.get() as u64,
-            id: c.event_id_counter,
-            forcer: self,
-            force_id: c.force_id_counter,
-        };
-        c.events.push(evt);
-    }
-    
-    fn read(self, c: &NodeCollection) -> LineState {
-        c.nodes[self.get()].get_input_state()
-    }
-}
+use base::{LineState, NodeIndex, NodeCreator, Element, NodeCollection, PropogationDelay};
 
 #[derive(Debug)]
 struct Nand {
     pub a: NodeIndex,
     pub b: NodeIndex,
     pub output: NodeIndex,
-}
-
-struct NodeCreator<'a> {
-    creation_index: usize,
-    elements: Vec<&'a (Element + 'a)>,
-    links: Vec<(NodeIndex, NodeIndex, PropogationDelay)>,
-}
-
-impl<'a> NodeCreator<'a> {
-
-    fn new(parent: &NodeCollection<'a>) -> NodeCreator<'a> {
-        NodeCreator{
-            creation_index: parent.nodes.len(),
-            elements: Vec::new(),
-            links: Vec::new()
-        }
-    }
-    
-    fn new_node(&mut self) -> NodeIndex {
-        let ret = self.creation_index;
-        self.creation_index += 1;
-        NodeIndex(ret)
-    }
-    
-    fn add_element(&mut self, elem: &'a (Element + 'a)) {
-        self.elements.push(elem);
-    }
-    
-    fn link(&mut self, a: NodeIndex, b: NodeIndex, delay: PropogationDelay) {
-        self.links.push((a, b, delay));
-    }
 }
 
 impl Nand {
@@ -375,7 +41,7 @@ impl Element for Nand {
             (LineState::High, LineState::High) => LineState::Low,
             _ => LineState::High
         };
-        println!("Running nand {:?}: {:?} {:?} -> {:?}", self, self.a.read(c), self.b.read(c), res);
+        //println!("Running nand {:?}: {:?} {:?} -> {:?}", self, self.a.read(c), self.b.read(c), res);
         self.output.write(res , c);
     }
     
@@ -467,6 +133,78 @@ impl NotSRLatch {
     }
 }
 
+
+#[derive(Debug)]
+struct DFlipFlop {
+    clock: NodeIndex,
+    data: NodeIndex,
+    q: NodeIndex,
+    not_q: NodeIndex,
+}
+
+impl DFlipFlop {
+    fn new<'a, 'b:'a>(creator: &mut NodeCreator<'a>, arena: &'b Arena) -> DFlipFlop {
+        let top = NotSRLatch::new(creator, arena);
+        let bottom = NotSRLatch::new(creator, arena);
+        let output = NotSRLatch::new(creator, arena);
+        let ander = AndGate::new(creator, arena);
+        
+        let clock = ander.a;
+        
+        creator.link(ander.b, top.not_q, STANDARD_DELAY);
+        creator.link(ander.output, bottom.not_s, STANDARD_DELAY);
+        let data = bottom.not_r;
+        
+        creator.link(bottom.q, output.not_r, STANDARD_DELAY);
+        creator.link(clock, top.not_r, STANDARD_DELAY);
+        creator.link(bottom.not_q, top.not_s, STANDARD_DELAY);
+        creator.link(top.not_q, output.not_s, STANDARD_DELAY);
+        
+        DFlipFlop {
+            clock: clock,
+            data: data,
+            q: output.q,
+            not_q: output.not_q
+        }
+    }
+}
+
+struct Register {
+    bits: Vec<DFlipFlop>,
+    clock: NodeIndex,
+}
+
+impl Register {
+    fn new<'a, 'b:'a>(creator: &mut NodeCreator<'a>, arena: &'b Arena, bit_count: usize) -> Register {
+        let bits : Vec<DFlipFlop> = range(0, bit_count).map(|_| { DFlipFlop::new(creator, arena) }).collect();
+        
+        let clock = bits[0].clock;
+        for bit in bits.slice_from(1).iter() {
+            creator.link(clock, bit.clock, STANDARD_DELAY);
+        }
+        
+        Register {
+            bits: bits,
+            clock: clock,
+        }
+    }
+    
+    fn read_u64(&self, c: &NodeCollection) -> Option<u64> {
+        let mut accum = 0u64;
+        for (index, bit) in self.bits.iter().enumerate() {
+            match bit.q.read(c) {
+                LineState::Low => {},
+                LineState::High => {
+                    accum |= 1<<index;
+                },
+                _ => { return None; }
+            }
+        }
+        
+        return Some(accum);
+    }
+}
+
 struct NWayAnd {
     inputs: Vec<NodeIndex>,
     output: NodeIndex,
@@ -543,19 +281,33 @@ fn main() {
     let power = arena.alloc(|| { Pin::new(&mut creator) });
     let ground = arena.alloc(|| { Pin::new(&mut creator) });
     let overall_output = arena.alloc(|| { Pin::new(&mut creator) });
+    let clock = arena.alloc(|| { Pin::new(&mut creator) });
+    let data = arena.alloc(|| { Pin::new(&mut creator) });
     
-    let not_s = arena.alloc(|| { Pin::new(&mut creator) });
-    let not_r = arena.alloc(|| { Pin::new(&mut creator) });
+    //let not_s = arena.alloc(|| { Pin::new(&mut creator) });
+    //let not_r = arena.alloc(|| { Pin::new(&mut creator) });
     
-    c.add_element(power);
-    c.add_element(ground);
-    c.add_element(overall_output);
-    c.add_element(not_s);
-    c.add_element(not_r);
+    creator.add_element(power);
+    creator.add_element(ground);
+    creator.add_element(overall_output);
+    creator.add_element(clock);
+    creator.add_element(data);
+    //c.add_element(not_s);
+    //c.add_element(not_r);
     
-    let sr1 = NotSRLatch::new(&mut creator, &arena);
-    creator.link(not_s.node, sr1.not_s, STANDARD_DELAY);
-    creator.link(not_r.node, sr1.not_r, STANDARD_DELAY);
+    //let sr1 = NotSRLatch::new(&mut creator, &arena);
+    //creator.link(not_s.node, sr1.not_s, STANDARD_DELAY);
+    //creator.link(not_r.node, sr1.not_r, STANDARD_DELAY);
+    
+    
+    let d = DFlipFlop::new(&mut creator, &arena);
+    creator.link(clock.node, d.clock, STANDARD_DELAY);
+    creator.link(data.node, d.data, STANDARD_DELAY);
+    
+    
+    let r = Register::new(&mut creator, &arena, 8);
+    creator.link(clock.node, r.clock, STANDARD_DELAY);
+    creator.link(power.node, r.bits[0].data, STANDARD_DELAY);
     
     /*let big_nander = NWayAnd::new_logtime(&mut creator, &arena, 50);
     
@@ -584,21 +336,22 @@ fn main() {
     
     power.node.write(LineState::High, &mut c);
     ground.node.write(LineState::Low, &mut c);
-    not_s.node.write(LineState::Low, &mut c);
-    not_r.node.write(LineState::High, &mut c);
+    data.node.write(LineState::Low, &mut c);
+    clock.node.write(LineState::Low, &mut c);
     
-    c.resolve_floats();
+    clock.node.write_later(LineState::High, PropogationDelay(1000), &mut c);
+    clock.node.write_later(LineState::Low, PropogationDelay(2000), &mut c);
     
-    not_s.node.write_later(LineState::High, PropogationDelay(1000), &mut c);
-    not_r.node.write_later(LineState::Low, PropogationDelay(2000), &mut c);
-    
+    //not_s.node.write_later(LineState::High, PropogationDelay(1000), &mut c);
+    //not_r.node.write_later(LineState::Low, PropogationDelay(2000), &mut c);
     
     loop {
         let more = c.play();
         if !more { break; }
     }
     
-    println!("q = {:?}, ~q = {:?}", sr1.q.read(&c), sr1.not_q.read(&c));
+    println!("q = {:?}, ~q = {:?}", d.q.read(&c), d.not_q.read(&c));
+    println!("r = {:?}", r.read_u64(&c));
     
     //println!("settled at t={}. out = {:?}", c.current_tick, overall_output.node.read(&c));
 }
